81acb1bc9761aa333a58980cae284f8655edb83e
setia <inchul9603@gmailc.com>
2022-10-04 00:36:15 +0900

    HelloController에 Dto사용 가능 추가
    RequestParam을 이용해 파라미터를 받아올 수 있도록 하는 형식 추가

--- src/main/java/com/awsProject/springboot/web/HelloController.java
+++ src/main/java/com/awsProject/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.awsProject.springboot.web;
 
+import com.awsProject.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // 컨트롤러를 JSON으로 반환하는 컨트롤러로 만들어줍니다
@@ -11,5 +13,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    // RequestParam 외부에서 넘긴 파라미터를 가져오는 어노테이션
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
