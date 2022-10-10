24a69feac21c506c781d0c36d85154c657f8ccad
setia <inchul9603@naver.com>
2022-10-10 22:47:54 +0900

    PostApiController 수정/조회 기능 추가

--- src/main/java/com/awsProject/springboot/web/PostsApiController.java
+++ src/main/java/com/awsProject/springboot/web/PostsApiController.java
@@ -3,9 +3,7 @@ package com.awsProject.springboot.web;
 import com.awsProject.springboot.service.posts.PostsService;
 import com.awsProject.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,4 +15,16 @@ public class PostsApiController {
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return PostsService.findById(id);˚
+    }
+
+
 }
