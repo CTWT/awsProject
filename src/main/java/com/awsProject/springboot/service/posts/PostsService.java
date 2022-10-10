7bde2ebb6f91567d9d2cdab208db4dd18182acec
setia <inchul9603@naver.com>
2022-10-10 23:00:41 +0900

    Postsservice 처리 항목 추가

--- src/main/java/com/awsProject/springboot/service/posts/PostsService.java
+++ src/main/java/com/awsProject/springboot/service/posts/PostsService.java
@@ -1,8 +1,11 @@
 package com.awsProject.springboot.service.posts;
 
 
+import com.awsProject.springboot.domain.posts.Posts;
 import com.awsProject.springboot.domain.posts.PostsRepository;
+import com.awsProject.springboot.web.dto.PostsResponseDto;
 import com.awsProject.springboot.web.dto.PostsSaveRequestDto;
+import com.awsProject.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,5 +20,19 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new
+                IllegalArgumentException("해당 게시글 존재 x id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다 id=" + id));
+        return new PostsResponseDto(entity);
     }
 }
