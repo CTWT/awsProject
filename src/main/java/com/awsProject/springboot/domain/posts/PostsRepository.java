3e7501045bcd8cd527a96fb0b35380fcfca6445e
setia <inchul9603@naver.com>
2022-10-11 00:29:52 +0900

    PostsRepository Query추가
    SpringDataJpa에서 제공하지 않는 메소드를 Query로 작성하는 방식의 예시

--- src/main/java/com/awsProject/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/awsProject/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,13 @@
 package com.awsProject.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // <Entity클래스, Pk타입>으로 상속하여 기본적인 CURD메소드가 자동생성
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
