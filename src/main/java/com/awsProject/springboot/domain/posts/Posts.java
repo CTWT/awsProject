5ddeff91d23ba2f9cf1357039f3024f7af06bd62
setia <inchul9603@naver.com>
2022-10-10 23:28:26 +0900

    JPA Auditing 기능 상속 처리
    모든 클래스에서 JPA Auditing사용 가능을 위해 어노테이션 추가

--- src/main/java/com/awsProject/springboot/domain/posts/Posts.java
+++ src/main/java/com/awsProject/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.awsProject.springboot.domain.posts;
 
+import com.awsProject.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter //클래스 내 모든 필드의 Getter메소드 자동 생성
 @NoArgsConstructor  //기본생성자 자동추가
 @Entity //테이블과 링크될 클래스임을 나타냄
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id //해당테이블의 pk필드를 나타냄
     @GeneratedValue(strategy = GenerationType.IDENTITY) // pk생성 규칙 스프링 2.0이상부터는 IDENTITY옵션이 있어야 auto incremnent가 됨
